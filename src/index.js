9755c69c6ee51b7b6319338284a40c3d1ed35e1a
Esteban Redón <esteban1.redon2@gmail.com>
2023-06-26 22:45:49 -0300

    Agregar un mensaje en el caso de que no sea una ruta conocida

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(indexRoutes)
 app.use('/api/employees', employeesRoutes)
 
+app.use((req, res, next) => {
+  res.status(404).send({message: 'endpoint not found'})
+})
 
+// Crear un servidor
 app.listen(app.get('port'), ()=>{
 
   console.log(`Servidor escuchando en el puerto ${app.get('port')}`)
